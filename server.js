fc7cea722d993f9f9ee2ab17a1b53dcf1ea68b00
MrSmarto <thijmensmarto@gmail.com>
2023-02-20 16:06:35 +0100

    little change server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.get('/', onhome)
   .listen(1900)
 
-app.use('/static',express.static('public'));
+app.use('/public',express.static('public'));
 
 function onhome(req, res) {
   res.send('<h1>Hello Client</h1>')
