b091e98781d20a5b2e20af172fcbd35c83251340
MrSmarto <thijmensmarto@gmail.com>
2023-02-23 11:18:27 +0100

    test

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express')
 const app = express();
-
-app.use((req, res, next) => {
-  res.status(404).send('<h1>DEZE PAGINA BESTAAT NIET</h1>');
-});
+const port = 1900;
 
 app.get('/', onhome)
   .listen(1900)
 
 app.use('/public',express.static('public'));
 
+app.use((req, res, next) => {
+  res.status(404).send('Resource not found');
+});
+
 function onhome(req, res) {
   res.send('<h1>Hello Client</h1>')
 }
+
