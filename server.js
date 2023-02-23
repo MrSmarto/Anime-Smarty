ad6a0630632299b51a446c9bf0841ac0a4958cec
MrSmarto <thijmensmarto@gmail.com>
2023-02-23 11:04:37 +0100

    jhgj

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
-
 const app = express();
 
-app.get('/', onhome)
+app.use((req, res, next) => {
+  res.status(404).send('<h1>DEZE PAGINA BESTAAT NIET</h1>');
+});
+
+app.get('/home', onhome)
   .listen(1900)
 
 app.use('/public',express.static('public'));
