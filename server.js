bf938b0031006d0fe54cb5cf53bdc3048fc6e5ac
MrSmarto <thijmensmarto@gmail.com>
2023-02-23 11:21:38 +0100

    test

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.get('/', onhome)
 
 app.use('/public',express.static('public'));
 
-app.use((req, res, next) => {
+app.get((req, res, next) => {
   res.status(404).send('Resource not found');
 });
 
