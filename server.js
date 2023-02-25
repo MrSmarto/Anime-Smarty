9dc8c181bc70782921e7d5ca0c093bee0cf97041
MrSmarto <thijmensmarto@gmail.com>
2023-02-25 17:27:23 +0100

    fixed hirachie

--- server.js
+++ server.js
@@ -2,20 +2,22 @@ const express = require('express')
 const app = express();
 const port = 1900;
 
-app.use('/public',express.static('public'));
 
-app.get('/overzichtpagina', onhome)
+app.use(express.static('static'));
+app.set('view engine', 'ejs');
 
-app.get('/detailfilm',)
-
-app.get('/profiel',)
-  
-app.get((req, res, next) => {
-  res.status(404).send('Resource not found');
+app.get('/',(req, res) => {
+  res.render('index.ejs');
 });
 
-function onhome(req, res) {
-  res.send('<h1>Hello Client</h1>')
-}
+app.use(function(req, res) {
+  res.status(404).render('404', {path:'Error'});
+});
 
-app.listen(1900)
+app.get("/liked", (req, res) => {
+  res.render("liked");
+});
+
+app.listen(port, () => {
+
+});
