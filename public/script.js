36b24aa2343244efc578e9aa3e8537f3d8678b5c
MrSmarto <thijmensmarto@gmail.com>
2023-03-12 18:34:54 +0100

    API werkend en gestyled

--- public/script.js
+++ public/script.js
@@ -1,31 +1,26 @@
-
-
-// BUTTON OP DE DETAILSPAGINA.EJS
+// BUTTON geeft aan dat je er op hebt geklikt.
 
 // selecteer de knop
 const button = document.querySelector('button');
 
 // voeg een event listener toe voor het "click" event
 button.addEventListener('click', function() {
-  // verander de achtergrondkleur van de body naar rood
-  document.body.style.backgroundColor = 'red';
 
-  // verander de tekst van de knop naar "Geklikt!"
-  button.textContent = 'Geklikt!';
+// verander de tekst van de knop naar "Geklikt!"
+button.textContent = 'Geklikt!';
 });
-
 
 // API
 
-const h1 = document.querySelector('section:nth-of-type(4) h1');
-const h2 = document.querySelector('section:nth-of-type(4) h2');
-const h3 = document.querySelector('section:nth-of-type(4) h3');
+const h1 = document.querySelector('section:nth-of-type(3) h1');
+const h2 = document.querySelector('section:nth-of-type(3) h2');
+const h3 = document.querySelector('section:nth-of-type(3) h3');
 
 fetch('https://animechan.vercel.app/api/random')
     .then(response => response.json())
     .then(data => {
   console.log(data);
-  h1.innerHTML = data.quote;
+  h1.innerHTML = data.anime;
   h2.innerHTML = data.character;
-  h3.innerHTML = data.anime;
+  h3.innerHTML = data.quote;
 })
